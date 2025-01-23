3f1cf1b93b751a02d6ec32e4d554550edf5e2437
luigi-giordano <luigi.giordano1992@gmail.com>
2025-01-23 17:11:05 +0100

    fix class active in bootstrap

--- src/components/ListMenu.jsx
+++ src/components/ListMenu.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom"
 
 function ListMenu() {
+
+
+
+
+
   return (
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
-        <ul class="navbar-nav">
-          <li class="nav-item mx-3">
-            <NavLink class="nav-link" to="/">HOME</NavLink>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+        <ul className="navbar-nav">
+          <li className="nav-item mx-3">
+            <NavLink className="nav-link text-primary" to="/">HOME</NavLink>
           </li>
-          <li class="nav-item mx-3">
-            <NavLink class="nav-link" to="/about">CHI SIAMO</NavLink>
+          <li className="nav-item mx-3">
+            <NavLink className="nav-link text-danger" to="/about">CHI SIAMO</NavLink>
           </li>
-          <li class="nav-item mx-3">
-            <NavLink class="nav-link" to="/posts">POSTS</NavLink>
+          <li className="nav-item mx-3">
+            <NavLink className="nav-link text-warning" to="/posts">POSTS</NavLink>
           </li>
         </ul>
       </nav>
